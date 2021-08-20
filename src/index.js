961fff6742e73943d819ae778229dc789c63f740
leosawka <56418748+leosawka@users.noreply.github.com>
2021-08-20 03:02:44 -0300

    configuring index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+//Initializations
 const server = express();
 //Settings
 server.set('port', process.env.PORT || 3000);
+server.set('path', path.join(__dirname, 'views'));
+server.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(server.get('views'), 'layouts'),
+  partialsDir: path.join(server.get('views'), 'partials'),
+  extname: '.hbs'
+}));
+server.set('view engine', '.hbs');
 //Middlewares
+server.use(express.utlencoded({extended: false}))
+server.use(methodOverride('_method'));
+server.use(session({
+  secret: 'encoder hash',
+  resave: true,
+  saveUninitialized: true
+}));
 
 //Global Variables
 
